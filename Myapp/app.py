dc2f2bd58eb3ec53844f51c4d7068ab6b47fc21d
daruomo <yumebito.0614@gmail.com>
2019-10-25 01:29:59 +0900

    layout第１案

--- Myapp/app.py
+++ Myapp/app.py
@@ -18,7 +18,7 @@ def index():
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html',
-            title='Login',
+            title='Adachi Quest',
             err=False,
             message='IDとパスワードを入力：',
             id='')
@@ -27,7 +27,6 @@ def login():
 @app.route('/login', methods=['POST'])
 def login_post():
     global member_data
-    print('Hello')
     id = request.form.get('id')
     pswd = request.form.get('pass')
     if id in member_data:
@@ -43,10 +42,29 @@ def login_post():
         return redirect('/')
     else:
         return render_template('login.html',
-                title='Login',
+                title='Adachi Quest',
                 err=False,
                 message='パスワードが違います。',
                 id=id)
+                
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    global member_data
+    id = request.form.get('id')
+    pswd = request.form.get('pass')
+    if id in member_data:
+        if pswd == member_data[id]:
+            session['login'] = True
+            flg = 'True'
+        else:
+            session['login'] = False
+            flg = 'False'
+    else:
+        member_data[id] = pswd
+        session['login'] = True
+        flg = 'True'
+    session['id'] = id
+    return flg
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True, use_debugger=False)
