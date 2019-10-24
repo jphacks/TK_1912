87487e34d6f7e82fcbaa9d5de8b6e1e1460e2bf6
daruomo <yumebito.0614@gmail.com>
2019-10-25 04:08:45 +0900

    ログイン機能追加

--- Myapp/app.py
+++ Myapp/app.py
@@ -1,70 +1,43 @@
 from flask import Flask, render_template, request, session, redirect, jsonify
+from lib.user_management import Management
 
 app = Flask(__name__)
 app.secret_key = b'random string...'
-member_data = {}
-
-@app.route('/', methods=['GET'])
-def index():
-    if 'login' in session and session['login']:
-        msg = 'Login id:'+session['id']
-        return render_template('index.html',
-                title='Messages',
-                message=msg)
-    else:
-        return redirect('/login')
 
 # login page access
-@app.route('/login', methods=['GET'])
+@app.route('/', methods=['GET'])
 def login():
     return render_template('login.html',
             title='Adachi Quest',
-            err=False,
-            message='IDとパスワードを入力：',
-            id='')
+            message='')
 
 
 @app.route('/login', methods=['POST'])
 def login_post():
-    global member_data
     id = request.form.get('id')
     pswd = request.form.get('pass')
-    if id in member_data:
-        if pswd == member_data[id]:
-            session['login'] = True
-        else:
-            session['login'] = False
-    else:
-        member_data[id] = pswd
-        session['login'] = True
-    session['id'] = id
-    if session['login']:
-        return redirect('/')
+    manager = Management(user_id=id, password=pswd)
+    # User & Password が一致するときのみTrue
+    flg = manager.checkLogin()
+    if flg:
+        return render_template('index.html',
+                title='Messages',
+                message='Hello',
+                user=id,
+                password=pswd)
     else:
         return render_template('login.html',
                 title='Adachi Quest',
-                err=False,
-                message='パスワードが違います。',
-                id=id)
+                message='パスワードが異なるか、登録がありません。') 
                 
 @app.route('/signup', methods=['POST'])
 def signup_post():
-    global member_data
-    id = request.form.get('id')
-    pswd = request.form.get('pass')
-    if id in member_data:
-        if pswd == member_data[id]:
-            session['login'] = True
-            flg = 'True'
-        else:
-            session['login'] = False
-            flg = 'False'
-    else:
-        member_data[id] = pswd
-        session['login'] = True
-        flg = 'True'
-    session['id'] = id
-    return flg
+    id = request.form.get('name')
+    pswd = request.form.get('password')
+    manager = Management(user_id=id, password=pswd)
+    # 登録済みのUserの場合はFasle
+    flg = manager.signup()
+    return str(flg)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True, use_debugger=False)
