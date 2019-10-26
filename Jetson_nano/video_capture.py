e7f9db1e3f9241308bd509d90ee3ba02676d4b5b
daruomo <yumebito.0614@gmail.com>
2019-10-26 15:57:44 +0900

    撮影待ち時間を変更

--- Jetson_nano/video_capture.py
+++ Jetson_nano/video_capture.py
@@ -11,26 +11,21 @@ def main():
 
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
     writer = cv2.VideoWriter('output.avi', fourcc, fps, (width, height))
-    pressed_key = False
-    t1 = 0
+    time.sleep(3)
+    t1 = time.time()
     while True:
         ret, frame = cam.read()
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            pressed_key = True
-            t1 = time.time()
+        t2 = time.time()
+        if (t2 - t1) > 5:
+            writer.write(frame)
+        else:
+            cv2.putText(frame,
+                "%d sec" % (5 - (t2 - t1) ),
+                (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                (0, 0, 0), 2)
 
-        if pressed_key:
-            t2 = time.time()
-            if (t2 - t1) > 10:
-                writer.write(frame)
-            else:
-                cv2.putText(frame,
-                    "残り: %f 秒" % (10 - (t2 - t1) ),
-                    (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    (0, 255, 0), 2)
-
-        if pressed_key and t2 - t1 > 15:
+        if cv2.waitKey(1) & 0xFF == ord('q') or t2 - t1 > 10:
             break
         
         cv2.imshow('frame', frame)
